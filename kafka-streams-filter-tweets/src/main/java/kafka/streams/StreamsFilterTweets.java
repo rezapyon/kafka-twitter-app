a7d8481e723a9ebaf76137238b15ed15b4d07f18
rezapyon <juliansyahmreza@gmail.com>
2022-01-08 09:13:32 +0700

    Add Kafka Streams

---
+++ kafka-streams-filter-tweets/src/main/java/kafka/streams/StreamsFilterTweets.java
@@ -0,0 +1,53 @@
+package kafka.streams;
+
+import com.google.gson.JsonParser;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+
+public class StreamsFilterTweets {
+
+    public static void main(String[] args) {
+        String bootstrapServers = "localhost:9092";
+        // create properties
+        Properties properties = new Properties();
+        properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "demo-kafka-streams");
+        properties.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
+        properties.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
+
+        // create a topology
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        // input topic
+        KStream<String, String> inputTopic = streamsBuilder.stream("twitter_tweets");
+        KStream<String, String> filteredStream = inputTopic.filter(
+                (k, jsonTweet) -> extractUserFollowersInTweet(jsonTweet) > 10000
+        );
+        filteredStream.to("important_tweets");
+
+        // build the topology
+        KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), properties);
+
+        // start streams application
+        kafkaStreams.start();
+    }
+
+    private static Integer extractUserFollowersInTweet(String tweetJson){
+        try {
+            return JsonParser.
+                    parseString(tweetJson).
+                    getAsJsonObject().
+                    get("user").
+                    getAsJsonObject().
+                    get("followers_count").
+                    getAsInt();
+        } catch (NullPointerException e) {
+            return 0;
+        }
+    }
+}
